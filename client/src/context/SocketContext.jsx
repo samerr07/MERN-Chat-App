9139050f12a00fbc307b0b17d9602647ff6815cb
Sameer Srivastava <112320710+samerr07@users.noreply.github.com>
2024-02-16 11:45:37 +0530

    Update SocketContext.jsx

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://mern-chat-app-delta-one.vercel.app/", {
+			const socket = io("https://mern-chat-app-delta-one.vercel.app", {
 				query: {
 					userId: authUser._id,
 				},
